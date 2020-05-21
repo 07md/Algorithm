531e6020a3acc9e93f922280945b785479029351
Alex <alex18812649207@gmail.com>
2020-05-21 23:26:26 +0800

    2020-5-21

--- LeetCode/Problems/1371. Find the Longest Substring Containing Vowels in Even Counts/1371. Find the Longest Substring Containing Vowels in Even Counts.py
+++ LeetCode/Problems/1371. Find the Longest Substring Containing Vowels in Even Counts/1371. Find the Longest Substring Containing Vowels in Even Counts.py
@@ -10,3 +10,19 @@
 # >>> Github    : https://github.com/koking0
 # >>> Blog      : https://alex007.blog.csdn.net/
 # ☆ ☆ ☆ ☆ ☆ ☆ ☆
+
+
+class Solution:
+	def findTheLongestSubstring(self, s: str) -> int:
+		mapper = {'a': 1, 'e': 2, 'i': 4, 'o': 8, 'u': 16}
+		seen = {0: -1}
+		res = cur = 0
+
+		for i in range(len(s)):
+			if s[i] in mapper:
+				cur ^= mapper.get(s[i])
+			if cur in seen:
+				res = max(res, i - seen.get(cur))
+			else:
+				seen[cur] = i
+		return res
--- LeetCode/Problems/5. Longest Palindromic Substring/5. Longest Palindromic Substring.py
+++ LeetCode/Problems/5. Longest Palindromic Substring/5. Longest Palindromic Substring.py
@@ -1,48 +1,67 @@
-# coding=utf-8
-'''
-Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
-给定字符串S，在S中找到最长回文子串，可以假定S的最大长度为1000。
-
-Example 1:
-
-    Input: "babad"
-    Output: "bab"
-    Note: "aba" is also a valid answer.
-
-Example 2:
-
-    Input: "cbbd"
-    Output: "bb"
-'''
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     : 2020/5/21 21:45
+# @File     : 5. Longest Palindromic Substring.py
+# ----------------------------------------------
+# ☆ ☆ ☆ ☆ ☆ ☆ ☆
+# >>> Author    : Alex 007
+# >>> QQ        : 2426671397
+# >>> Mail      : alex18812649207@gmail.com
+# >>> Github    : https://github.com/koking0
+# >>> Blog      : https://alex007.blog.csdn.net/
+# ☆ ☆ ☆ ☆ ☆ ☆ ☆
 
 
 class Solution:
-    def longestPalindrome(self, s: str) -> str:
-        if len(s) == 1:
-            return s
+	def longestPalindrome_violence(self, s: str) -> str:
+		if len(s) == 1:
+			return s
 
-        for i in range(int(len(s) / 2)):
-            if s[i] != s[len(s) - i -1]:
-                break
-        else:
-            return s
-        
-        substring = ""
+		for i in range(int(len(s) / 2)):
+			if s[i] != s[len(s) - i - 1]:
+				break
+		else:
+			return s
 
-        for i in range(len(s)):
-            for j in range(len(s)):
-                if j > i:
-                    substring_temp = s[i:j]
-                    print(substring_temp)
-                    
-                    for k in range(int(1 + (len(substring_temp)) / 2)):
-                        if substring_temp[k] != substring_temp[len(substring_temp) - k -1]:
-                            break
-                    else:
-                        print("enable: ", substring_temp)
-                        if len(substring_temp) > len(substring):
-                            substring = substring_temp
-        return substring
+		substring = ""
 
-a = Solution()
-print(a.longestPalindrome("abb"))
+		for i in range(len(s)):
+			for j in range(i + 1, len(s) + 1):
+				substring_temp = s[i:j]
+				substring_length = len(substring_temp)
+
+				for k in range(int(1 + substring_length / 2)):
+					if substring_temp[k] != substring_temp[substring_length - k - 1]:
+						break
+				else:
+					if substring_length > len(substring):
+						substring = substring_temp
+		return substring
+
+	def longestPalindrome_manacher(self, s: str) -> str:
+		def expand(s, left, right):
+			while left > -1 and right < len(s) and s[left] == s[right]:
+				left -= 1
+				right += 1
+			return (right - left - 2) // 2
+
+		end, start = -1, 0
+		s = '#' + '#'.join(list(s)) + '#'
+		arm_len = []
+		right = -1
+		j = -1
+		for i in range(len(s)):
+			if right >= i:
+				i_sym = 2 * j - i
+				min_arm_len = min(arm_len[i_sym], right - i)
+				cur_arm_len = expand(s, i - min_arm_len, i + min_arm_len)
+			else:
+				cur_arm_len = expand(s, i, i)
+			arm_len.append(cur_arm_len)
+			if i + cur_arm_len > right:
+				j = i
+				right = i + cur_arm_len
+			if 2 * cur_arm_len + 1 > end - start:
+				start = i - cur_arm_len
+				end = i + cur_arm_len
+		return s[start + 1:end + 1:2]
