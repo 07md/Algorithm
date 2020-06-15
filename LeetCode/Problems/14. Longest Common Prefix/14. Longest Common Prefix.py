6aee52e46a4904562adb0b59d97d82c64cc2694d
Alex <alex18812649207@gmail.com>
2020-06-15 08:21:08 +0800

    2020-6-15

---
+++ LeetCode/Problems/14. Longest Common Prefix/14. Longest Common Prefix.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     : 2020/6/15 8:19
+# @File     : 14. Longest Common Prefix.py
+# ----------------------------------------------
+# ☆ ☆ ☆ ☆ ☆ ☆ ☆ 
+# >>> Author    : Alex 007
+# >>> QQ        : 2426671397
+# >>> Mail      : alex18812649207@gmail.com
+# >>> Github    : https://github.com/koking0
+# >>> Blog      : https://alex007.blog.csdn.net/
+# ☆ ☆ ☆ ☆ ☆ ☆ ☆
