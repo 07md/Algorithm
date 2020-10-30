3640869bd7bef5c02ee70a641d064e32feefb51f
Alex 007 <alex18812649207@gmail.com>
2020-10-30 12:42:40 +0800

    2020-10-30

---
+++ BlueBridgeCup/Subject/2017/National _C_C++_C/1/code.py
@@ -0,0 +1,17 @@
+import math
+
+
+def isPrimeNumber(num: int) -> bool:
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+
+m, n = 0, int(input())
+for i in range(4, n, 2):
+    for j in range(2, int(i / 2)):
+        if isPrimeNumber(j) and isPrimeNumber(i - j):
+            m = max(m, j)
+            break
+print(m)
---
+++ BlueBridgeCup/Subject/2020-/7月/Province_C_C++_A/3/code.py
@@ -0,0 +1,16 @@
+def dfs(index, num):
+    if index >= 15:
+        global minn
+        minn = min(minn, abs(sum(masks) - 2 * num))
+        print(minn)
+        return
+    dfs(index + 1, num)
+    dfs(index + 1, num + masks[index])
+
+
+if __name__ == '__main__':
+    masks = [9090400, 8499400, 5926800, 8547000, 4958200, 4422600, 5751200, 4175600, 6309600, 5865200, 6604400, 4635000,
+             10663400, 8087200, 4554000]
+    minn = sum(masks)
+    dfs(0, 0)
+    print(minn)
