2ea2ef6c8a99094fa86db565b2f902baa6647092
Alex 007 <alex18812649207@gmail.com>
2020-12-20 23:27:36 +0800

    2020-12-20

--- Learn/宏观调度/Rotate matrix/code.py
+++ Learn/宏观调度/Rotate matrix/code.py
@@ -13,18 +13,18 @@
 # ☆ ☆ ☆ ☆ ☆ ☆ ☆ 
 
 
-def rotate_edge(input_matrix, left_row, left_col, right_row, right_co1):
+def rotate_edge(matrix, left_row, left_col, right_row, right_co1):
     for index in range(right_co1 - left_col):
-        [input_matrix[left_row][left_col + index], input_matrix[right_row - index][left_col],
-         input_matrix[right_row][right_co1 - index], input_matrix[left_row + index][right_co1]] \
-            = [input_matrix[right_row - index][left_col], input_matrix[right_row][right_co1 - index],
-               input_matrix[left_row + index][right_co1], input_matrix[left_row][left_col + index]]
+        [matrix[left_row][left_col + index], matrix[right_row - index][left_col],
+         matrix[right_row][right_co1 - index], matrix[left_row + index][right_co1]] \
+            = [matrix[right_row - index][left_col], matrix[right_row][right_co1 - index],
+               matrix[left_row + index][right_co1], matrix[left_row][left_col + index]]
 
 
-def rotate_matrix(input_matrix):
-    left_row, left_col, right_row, right_col = 0, 0, len(input_matrix) - 1, len(input_matrix[0]) - 1
+def rotate_matrix(matrix):
+    left_row, left_col, right_row, right_col = 0, 0, len(matrix) - 1, len(matrix[0]) - 1
     while left_row <= right_row and left_col <= right_col:
-        rotate_edge(input_matrix, left_row, left_col, right_row, right_col)
+        rotate_edge(matrix, left_row, left_col, right_row, right_col)
         left_row += 1
         left_col += 1
         right_row -= 1
@@ -32,13 +32,13 @@ def rotate_matrix(input_matrix):
 
 
 if __name__ == '__main__':
-    matrix = [[1, 2, 3, 4],
-              [5, 6, 7, 8],
-              [9, 10, 11, 12],
-              [13, 14, 15, 16]]
-    for i in matrix:
+    m = [[1, 2, 3, 4],
+         [5, 6, 7, 8],
+         [9, 10, 11, 12],
+         [13, 14, 15, 16]]
+    for i in m:
         print(i)
     print('-----------')
-    rotate_matrix(matrix)
-    for i in matrix:
+    rotate_matrix(m)
+    for i in m:
         print(i)
