0b6bd56a6572c9ccff747d3b91800f79645f351a
Alex 007 <alex18812649207@gmail.com>
2021-01-28 10:42:19 +0800

    2021-1-28

--- BlueBridgeCup/Subject/2014-/Province_C_C++_C/3/code.py
+++ BlueBridgeCup/Subject/2014-/Province_C_C++_C/3/code.py
@@ -1,11 +1,11 @@
 import string
 
 
-# a_s = string.ascii_lowercase[:19]
 """19本来就是奇数个，每次删除奇数位之后还是奇数个，所以只在19位之内模拟也是可以的。"""
-a_s = string.ascii_lowercase[:19] * 106
-while len(a_s) > 1:
-    for i in range(len(a_s)):
+s = string.ascii_lowercase[:19] * 106
+print(s)
+while len(s) > 1:
+    for i in range(len(s)):
         if i & 1:
-            a_s = a_s[:i] + a_s[i + 1:]
-print(a_s)
+            s = s[:i] + s[i + 1:]
+print(s)
