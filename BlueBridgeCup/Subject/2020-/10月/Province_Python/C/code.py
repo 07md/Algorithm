b56425827f7a5ebf4b1aabdf74c83c1590e11614
Alex 007 <alex18812649207@gmail.com>
2021-02-06 10:11:39 +0800

    2021-2-6

---
+++ BlueBridgeCup/Subject/2020-/10月/Province_Python/C/code.py
@@ -0,0 +1,11 @@
+from datetime import date
+from datetime import timedelta
+
+start = date(2000, 1, 1)
+end = date(2020, 10, 2)
+
+res = 0
+while start < end:
+    res += 2 if start.day == 1 or start.weekday() == 0 else 1
+    start += timedelta(days=1)
+print(res)
