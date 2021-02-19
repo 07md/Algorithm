1f7a120d262b80f2b4fc452a33f698ccdd9e1fad
Alex 007 <alex18812649207@gmail.com>
2021-02-19 10:29:14 +0800

    2021-02-19

--- BlueBridgeCup/Subject/2017/National _C_C++_C/1/code.py
+++ BlueBridgeCup/Subject/2017/National _C_C++_C/1/code.py
@@ -1,6 +1,3 @@
-import math
-
-
 def isPrimeNumber(num: int) -> bool:
     for i in range(2, num):
         if num % i == 0:
@@ -8,10 +5,11 @@ def isPrimeNumber(num: int) -> bool:
     return True
 
 
-m, n = 0, int(input())
-for i in range(4, n, 2):
-    for j in range(2, int(i / 2)):
-        if isPrimeNumber(j) and isPrimeNumber(i - j):
-            m = max(m, j)
-            break
-print(m)
+if __name__ == '__main__':
+    m, n = 0, int(input())
+    for i in range(4, n, 2):
+        for j in range(2, int(i / 2)):
+            if isPrimeNumber(j) and isPrimeNumber(i - j):
+                m = max(m, j)
+                break
+    print(m)
