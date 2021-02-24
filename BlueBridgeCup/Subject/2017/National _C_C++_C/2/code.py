15acaa0a70aed4272db0393af68bc9aad62afd3d
Alex 007 <alex18812649207@gmail.com>
2021-02-24 15:40:34 +0800

    2020-2-24

--- BlueBridgeCup/Subject/2017/National _C_C++_C/2/code.py
+++ BlueBridgeCup/Subject/2017/National _C_C++_C/2/code.py
@@ -1,7 +1,6 @@
 import itertools
 
-
-nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 for item in itertools.combinations(nums, 8):
     a = list(item)
     b = list(set(nums) ^ set(item))
