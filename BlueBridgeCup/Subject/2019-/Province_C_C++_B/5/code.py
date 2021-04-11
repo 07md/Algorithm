a7d1cd34acb3f674ac59fa3b705e43229a2c9e78
Alex007 <alex18812649207@gmail.com>
2021-04-11 18:43:02 +0800

    2021-04-11

--- BlueBridgeCup/Subject/2019-/Province_C_C++_B/5/code.py
+++ BlueBridgeCup/Subject/2019-/Province_C_C++_B/5/code.py
@@ -1,18 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Time     : 2020/7/19 10:13
-# @File     : code.py
-# ----------------------------------------------
-# ☆ ☆ ☆ ☆ ☆ ☆ ☆ 
-# >>> Author    : Alex 007
-# >>> QQ        : 2426671397
-# >>> WeChat    : Alex-Paddle
-# >>> Mail      : alex18812649207@gmail.com
-# >>> Github    : https://github.com/koking0
-# >>> Blog      : https://alex007.blog.csdn.net/
-# ☆ ☆ ☆ ☆ ☆ ☆ ☆
-
-
 class Node:
 	def __init__(self, x, y, path):
 		self.x = x
