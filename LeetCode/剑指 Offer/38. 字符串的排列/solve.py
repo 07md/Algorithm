cfda0d20206844bf928000aecceb67feacc4eeeb
liuzhaofeng1 <liu_zhao_feng_alex@163.com>
2021-06-22 18:11:26 +0800

    2021-06-22

---
+++ LeetCode/剑指 Offer/38. 字符串的排列/solve.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/6/22 18:06
+# @Author  : Alex
+# @File    : solve.py
+# @Software: PyCharm
+# ☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆
+from typing import List
+from itertools import permutations
+
+
+class Solution:
+    def permutation(self, s: str) -> List[str]:
+        ans = set()
+        for item in permutations(s):
+            ans.add(''.join(item))
+        return list(ans)
+
+
+if __name__ == '__main__':
+    print(Solution().permutation("abc"))
