0481deb3f3559743eac573baa4965b9d785c6f40
liuzhaofeng1 <liu_zhao_feng_alex@163.com>
2021-06-29 12:55:30 +0800

    2021-06-29

---
+++ LeetCode/Problems/168. Excel Sheet Column Title/solve.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/6/29 9:13
+# @Author  : Alex
+# @File    : solve.py
+# @Software: PyCharm
+# ☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆
+class Solution:
+    def convertToTitle(self, columnNumber: int) -> str:
+        ans = list()
+        while columnNumber > 0:
+            a0 = (columnNumber - 1) % 26 + 1
+            ans.append(chr(a0 - 1 + ord("A")))
+            columnNumber = (columnNumber - a0) // 26
+        return "".join(ans[::-1])
+
+
+if __name__ == '__main__':
+    print(Solution().convertToTitle(701))
---
+++ LeetCode/Problems/1828. Queries on Number of Points Inside a Circle/solve.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/6/25 9:26
+# @Author  : Alex
+# @File    : solve.py
+# @Software: PyCharm
+# ☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆☆
+from typing import List
+
+
+class Solution:
+    def countPoints(self, points: List[List[int]], queries: List[List[int]]) -> List[int]:
+
