a8752bb9aadbe595cd17bfa213aef8fba06d95b0
Alex007 <alex18812649207@gmail.com>
2021-07-26 23:09:23 +0800

    2021-07-26

--- LeetCode/Problems/1052. Grumpy Bookstore Owner/code.py
+++ LeetCode/Problems/1052. Grumpy Bookstore Owner/code.py
@@ -7,12 +7,12 @@ class Solution:
         找到一个时间长度为 X 的窗口包含最多原本因为老板生气而不满意的顾客（Sliding Window）
         ans = 老板不生气时间内的顾客总数（customer1） + 窗口 X 内的原本不满意的顾客（customer2）
         """
-        N = len(customers)
-        customer1 = sum(customers[i] * (1 - grumpy[i]) for i in range(N))
-        customer2 = customerX = sum(customers[i] * grumpy[i] for i in range(X))
-        for i in range(X, N):
-            customerX = customerX + customers[i] * grumpy[i] - customers[i - X] * grumpy[i - X]
-            customer2 = max(customer2, customerX)
+        n = len(customers)
+        customer1 = sum(customers[i] * (1 - grumpy[i]) for i in range(n))
+        customer2 = customer_x = sum(customers[i] * grumpy[i] for i in range(X))
+        for i in range(X, n):
+            customer_x = customer_x + customers[i] * grumpy[i] - customers[i - X] * grumpy[i - X]
+            customer2 = max(customer2, customer_x)
         return customer1 + customer2
 
 
