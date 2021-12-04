8a765785c1ec60435ec8166e6295888b5a87f203
Alex007 <alex18812649207@gmail.com>
2021-12-04 20:43:59 +0800

    2021-12-4

--- Learn/DataStructure/Graph/TopologicalSorting/template.cpp
+++ Learn/DataStructure/Graph/TopologicalSorting/template.cpp
@@ -1,51 +1,105 @@
 #include <list>
 #include <queue>
 #include <iostream>
+#include <algorithm>
 
 using  namespace std;
 
 class Graph {
-	int V;				// 顶点个数 
-	int* indegree;		// 顶点入度 
-	list<int> *adj;		// 邻接表 
+	int V;					// 顶点个数 
+	int* inDegree;			// 顶点入度
+	list<int> *adj;			// 邻接表 
+	
+	int* visit;						// 拓扑排序时DFS标记顶点是否访问过 
 	
 	public:
+		bool haveLoop = false;	// 判断是否有环 
+		vector<int> topological_seq;	// 拓扑序列 
+		
 	Graph(int v) {
 		this->V = v;
 		this->adj = new list<int>[v];
-		this->indegree = new int[v];
+		this->inDegree = new int[v];
+		this->visit = new int[v];
 		for(int i = 0; i < v; i++) {
-			this->indegree[i] = 0;
+			this->inDegree[i] = 0;
+			this->visit[i] = 0;
 		}
 	}
 	~Graph() {
 		delete [] this->adj;
-		delete [] this->indegree;
+		delete [] this->inDegree;
 	}
 	void addEdge(int v, int w) {
 		this->adj[v].push_back(w);
-		this->indegree[w]++;
+		this->inDegree[w]++;
 	}
-	vector<int> topologicalSort() {
-		vector<int> res;	// 拓扑序列 
+	
+	void topologicalSort_BFS() {
+		/*
+			BFS Kahn算法，基于贪心，每次从入度为0的点开始，正序为拓扑序列 
+		*/
+		vector <int>().swap(this->topological_seq);	// 清空拓扑序列 
+		
 		queue<int> que;		// 入度为0顶点集合 
 		
+		// 从图中选择入度为0的点 
 		for(int i = 0; i < this->V; i++) {
-			if (this->indegree[i] == 0) {
+			if (this->inDegree[i] == 0) {
 				que.push(i);
 			}
 		}
+		// BFS 
 		while(!que.empty()) {
 			int front = que.front();
 			que.pop();
-			res.push_back(front);
+			this->topological_seq.push_back(front);
 			for(int item:this->adj[front]) {
-				if (!(--this->indegree[item])) {
+				if (!(--this->inDegree[item])) {
 					que.push(item);
 				}
 			}
 		}
-		return res.size() == this->V ? res : vector<int> ();
+		this->haveLoop = this->topological_seq.size() == this->V ? false : true;
+	}
+	void topologicalSort_DFS() {
+		/*
+			基于搜索，每次保证当前点出度为0后才遍历，逆序为拓扑序列 
+			单纯拓扑不推荐DFS，如果是二分图才用DFS 
+			// visit 0 - no visit; 1 - in progress; 2 - visit
+		*/
+		vector <int>().swap(this->topological_seq);	// 清空拓扑序列 
+		
+		for (int i = 0; i < this->V; i++) {
+			if (!this->visit[i]) {  
+				dfs(i);
+				if (this->haveLoop) return;
+			}
+		}
+		reverse(this->topological_seq.begin(), this->topological_seq.end());
+	}
+	void dfs(int v) {
+		for (auto u: this->adj[v]) {
+			if (this->visit[u] == 0) {		// 未搜索，继续深入 
+				dfs(u);
+				if (this->haveLoop) return;
+			} else if (this->visit[u] == 2) {	// 遇到环，退出 
+				this->haveLoop = true;
+				return;
+			}
+		}
+		this->visit[v] = 1;
+		this->topological_seq.push_back(v);
+	}
+	void printTopologicalSeq() {
+	    if (!this->haveLoop) {
+		    for (int item:this->topological_seq) {
+		    	cout << item << " ";
+			}
+		} else {
+			cout << "Graph have loop!";
+		}
+		cout << endl;
 	}
 };
 
@@ -63,8 +117,11 @@ int main(){
     graph.addEdge(6,7);
     graph.addEdge(5,7);
     graph.addEdge(3,7);
-    for (int item:graph.topologicalSort()) {
-    	cout << item << " ";
-	}
+    
+    graph.topologicalSort_BFS();
+	graph.printTopologicalSeq();
+	
+	graph.topologicalSort_DFS();
+	graph.printTopologicalSeq();
     return 0;
 }
