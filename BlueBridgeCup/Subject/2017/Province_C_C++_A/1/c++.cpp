0cd72467eb41d11b779b2a44baeb2c9432e71d2e
liuzhaofeng1 <Alexlzf518713>
2022-01-17 09:06:50 +0800

    2022-01-17

--- BlueBridgeCup/Subject/2017/Province_C_C++_A/1/c++.cpp
+++ BlueBridgeCup/Subject/2017/Province_C_C++_A/1/c++.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+
 using namespace std;
+
 char map[11][11]=
 					{
 						{' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' '},
@@ -17,30 +19,28 @@ char map[11][11]=
 						{' ','U','L','R','D','L','U','U','R','R','R'}
 					};
 	int people[11][11],ans=0;
-bool dfs(int i,int j)
-{
-	if(i>10||j>10||i<1||j<1) return true;
-	if(people[i][j]==1) return false;
-	people[i][j]=1;
-	switch(map[i][j])
-	{
-		case 'U':return dfs(i-1,j);
-		case 'D':return dfs(i+1,j);
-		case 'R':return dfs(i,j+1);
-		case 'L':return dfs(i,j-1);
+
+bool dfs(int i,int j) {
+	if(i > 10 || j > 10 || i < 1 || j < 1) return true;
+	if(people[i][j] == 1) return false;
+
+	people[i][j] = 1;
+	switch(map[i][j]) {
+		case 'U':return dfs(i - 1, j);
+		case 'D':return dfs(i + 1, j);
+		case 'R':return dfs(i, j + 1);
+		case 'L':return dfs(i, j - 1);
 		default: return false;
 	}
 }
-int main()
-{
- 	for(int i=1;i<=10;i++)
-	{
-		for(int j=1;j<=10;j++)
-		{
-			memset(people,0,sizeof(people));
-			if(dfs(i,j)) ans++;
+
+int main() {
+ 	for(int i = 1; i <= 10; i++) {
+		for(int j = 1; j <= 10; j++) {
+			memset(people, 0, sizeof(people));
+			if(dfs(i, j)) ans++;
 		}
 	}
-	cout<<ans<<endl;
+	cout << ans << endl;
 	return 0;
 }
